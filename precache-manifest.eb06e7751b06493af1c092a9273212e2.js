b67caeb0f5fed06ff2e6b97ba99273e2cb48a0f2
Daniel Don <dandons25@gmail.com>
2019-05-01 14:05:13 +0100

    Updates

---
+++ precache-manifest.eb06e7751b06493af1c092a9273212e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "66395e9027b8eb40f6b4",
+    "url": "/cryptocheck/static/js/runtime~main.29db208a.js"
+  },
+  {
+    "revision": "7e2d3281d67473abb28d",
+    "url": "/cryptocheck/static/js/main.dc5bc8a2.chunk.js"
+  },
+  {
+    "revision": "0678b9e4af12e3c7f24e",
+    "url": "/cryptocheck/static/js/2.6d83fd0c.chunk.js"
+  },
+  {
+    "revision": "7e2d3281d67473abb28d",
+    "url": "/cryptocheck/static/css/main.79dd1883.chunk.css"
+  },
+  {
+    "revision": "c718175cae71ffde78d6ca47cc0d7f4f",
+    "url": "/cryptocheck/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/cryptocheck/precache-manifest.eb06e7751b06493af1c092a9273212e2.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/cryptocheck/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
